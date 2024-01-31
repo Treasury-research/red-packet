7be19aaa4c063044ce9b99d612b898c187a0b7ce
Terence Ge <terencegehui@yahoo.com>
2024-01-31 20:25:00 +0800

    chore: add tmg sdk

--- src/pages/Root/index.jsx
+++ src/pages/Root/index.jsx
@@ -2,42 +2,152 @@ import { Box } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom';
 import CloseIcon from "@/Components/Icons/Close"
 import MoreIcon from "@/Components/Icons/More"
+import {
+  SDKProvider,
+  useMainButton,
+  useBackButton,
+  useInitData,
+  DisplayGate,
+} from '@tma.js/sdk-react'
+import { useEffect, useMemo, useState } from 'react'
+
+function MainButton() {
+  const mb = useMainButton()
+  const bb = useBackButton()
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const removeMainButtonClick = mb.on('click', () => {
+      setCount((prevCount) => prevCount + 1)
+    })
+
+    const removeBackButtonClick = bb.on('click', () => {
+      setCount((prevCount) => prevCount - 1)
+    })
+
+    return () => {
+      removeMainButtonClick()
+      removeBackButtonClick()
+    }
+  }, [])
+
+  useEffect(() => {
+    mb.enable().show()
+  }, [])
+
+  useEffect(() => {
+    mb.setText(`Count is ${count}`)
+  }, [mb, count])
+
+  useEffect(() => {
+    if (count === 0) {
+      bb.hide()
+      return
+    }
+
+    bb.show()
+  }, [bb, count])
+
+  return null
+}
+
+function InitData() {
+  const initData = useInitData()
+
+  const initDataJson = useMemo(() => {
+    if (!initData) {
+      return 'Init data is empty.'
+    }
+
+    const { authDate, chat, hash, canSendAfter, queryId, receiver, user, startParam } = initData
+
+    return JSON.stringify({
+      authDate,
+      chat,
+      hash,
+      canSendAfter,
+      queryId,
+      receiver,
+      user,
+      startParam,
+    }, null, ' ')
+  }, [initData])
+
+  return (
+    <pre>
+      <code>
+        {initDataJson}
+      </code>
+    </pre>
+  )
+}
+
+function SDKProviderError({ error }) {
+  return (
+    <div>
+      Oops. Something went wrong.
+      <blockquote>
+        <code>
+          {error instanceof Error ? error.message : JSON.stringify(error)}
+        </code>
+      </blockquote>
+    </div>
+  )
+}
+
+function SDKProviderLoading() {
+  return <div>SDK is loading.</div>
+}
+
+function SDKInitialState() {
+  return <div>Waiting for initialization to start.</div>
+}
 
 export default function Root() {
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      background="black"
-    >
-      <Box
-        position="fixed"
-        top="0"
-        left="0"
-        height="44px"
-        width="100vw"
-        background="#212121"
-        padding="0 24px"
-        display="flex"
-        alignItems="center"
-        justifyContent="flex-start"
+    <SDKProvider options={{ acceptCustomStyles: true, cssVars: true, async: true }}>
+      <DisplayGate
+        error={SDKProviderError}
+        loading={SDKProviderLoading}
+        initial={SDKInitialState}
       >
-        <Box marginRight="20px" cursor="pointer">
-          <CloseIcon />
-        </Box>
         <Box
-          color="white"
-          fontWeight="600"
+          width="100vw"
+          height="100vh"
+          background="black"
         >
-          typobot
+          <Box
+            position="fixed"
+            top="0"
+            left="0"
+            height="44px"
+            width="100vw"
+            background="#212121"
+            padding="0 24px"
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-start"
+          >
+            <Box marginRight="20px" cursor="pointer">
+              <CloseIcon />
+            </Box>
+            <Box
+              color="white"
+              fontWeight="600"
+            >
+              typobot
+            </Box>
+            <Box marginLeft="auto" cursor="pointer">
+              <MoreIcon />
+            </Box>
+          </Box>
+          <Box paddingTop="44px" height="100%">
+            <Outlet />
+          </Box>
         </Box>
-        <Box marginLeft="auto" cursor="pointer">
-          <MoreIcon />
-        </Box>
-      </Box>
-      <Box paddingTop="44px" height="100%">
-        <Outlet />
-      </Box>
-    </Box>
+        <MainButton />
+        <InitData />
+      </DisplayGate>
+    </SDKProvider>
   )
 }
