8a1b659822fd0f9ada50eaa79891f1e244a2472c
Terence Ge <terencegehui@yahoo.com>
2024-02-03 14:54:43 +0800

    update

--- src/pages/Root/index.jsx
+++ src/pages/Root/index.jsx
@@ -71,13 +71,7 @@ function InitData() {
     })
   }, [initDataRaw])
 
-  return (
-    <pre>
-      <code>
-        {initDataJson}:{initDataRaw}
-      </code>
-    </pre>
-  )
+  return null
 }
 
 function SDKProviderError({ error }) {
@@ -102,43 +96,43 @@ function SDKInitialState() {
 }
 
 export default function Root() {
-  return (
-    <Box
-      width="100vw"
-      height="100vh"
-      background="black"
-    >
-      <Box
-        position="fixed"
-        top="0"
-        left="0"
-        height="44px"
-        width="100vw"
-        background="#212121"
-        padding="0 24px"
-        display="flex"
-        alignItems="center"
-        justifyContent="flex-start"
-      >
-        <Box marginRight="20px" cursor="pointer">
-          <CloseIcon />
-        </Box>
-        <Box
-          color="white"
-          fontWeight="600"
-        >
-          typobot
-        </Box>
-        <Box marginLeft="auto" cursor="pointer">
-          <MoreIcon />
-        </Box>
-      </Box>
-      <Box paddingTop="44px" height="100%">
-        <Outlet />
-      </Box>
-    </Box>
-  )
-
+  /* return (
+   *   <Box
+   *     width="100vw"
+   *     height="100vh"
+   *     background="black"
+   *   >
+   *     <Box
+   *       position="fixed"
+   *       top="0"
+   *       left="0"
+   *       height="44px"
+   *       width="100vw"
+   *       background="#212121"
+   *       padding="0 24px"
+   *       display="flex"
+   *       alignItems="center"
+   *       justifyContent="flex-start"
+   *     >
+   *       <Box marginRight="20px" cursor="pointer">
+   *         <CloseIcon />
+   *       </Box>
+   *       <Box
+   *         color="white"
+   *         fontWeight="600"
+   *       >
+   *         typobot
+   *       </Box>
+   *       <Box marginLeft="auto" cursor="pointer">
+   *         <MoreIcon />
+   *       </Box>
+   *     </Box>
+   *     <Box paddingTop="44px" height="100%">
+   *       <Outlet />
+   *     </Box>
+   *   </Box>
+   * )
+   */
   return (
     <SDKProvider options={{ acceptCustomStyles: true, cssVars: true, async: true }}>
       <DisplayGate
