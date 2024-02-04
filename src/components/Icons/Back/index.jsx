07dc682a28e79c3979426d211f4d265317b122e6
Terence Ge <terencegehui@yahoo.com>
2024-02-04 17:04:20 +0800

    update

---
+++ src/components/Icons/Back/index.jsx
@@ -0,0 +1,10 @@
+const Back = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <rect opacity="0.01" width="24" height="24" fill="#D8D8D8"/>
+      <path d="M10 19L3 12L10 5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+
+export default Back
