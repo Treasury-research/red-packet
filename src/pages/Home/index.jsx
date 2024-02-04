3b1ca5a83e3c3c2eda296287918229eaa639bf74
Terence Ge <terencegehui@yahoo.com>
2024-02-04 19:41:39 +0800

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -71,7 +71,7 @@ export default function Home() {
     )
   }
 
-  if (userInfo && !userInfo.address) {
+  if (userInfo && !!userInfo.account && !userInfo.address) {
     return (
       <BindAddress />
     )
