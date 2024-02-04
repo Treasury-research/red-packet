65e522c1e984ae85c18e76bc904b05e379e705b3
Terence Ge <terencegehui@yahoo.com>
2024-02-04 16:53:04 +0800

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -132,6 +132,9 @@ export default function Home() {
               </Box>
             </Box>
           </Box>
+          <Box>
+            {JSON.stringify(userInfo)}
+          </Box>
           {!isSignedIn && (
             <Box width="100%" marginBottom="40px" marginTop="auto">
               <Button width="100%" borderRadius="50px" height="50px" fontSize="16px" fontWeight="bold" onClick={signIn} loading={isLogingIn} disabled={isLogingIn}>
